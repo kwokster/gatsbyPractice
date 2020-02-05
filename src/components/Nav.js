e0c68c768ee4ac62b73f7e23e502b0185f0fada2
Jon Kwok <jkwokcl@gmail.com>
2020-02-04 19:35:24 -0500

    nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,49 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
+import { FaAlignRight } from "react-icons/fa"
+import styles from "../css/navbar.module.css"
+import links from "../constants/links"
+import socialIcons from "../constants/social-icons"
+import logo from "../images/logo.svg"
 
 const Nav = () => {
+  const [isOpen, setNav] = useState(false)
+  const toggleNav = () => {
+    setNav(isOpen => !isOpen)
+  }
   return (
-    <div>
-      <h1>Nav Bar</h1>
-    </div>
+    <nav className={styles.navbar}>
+      <div className={styles.navCenter}>
+        <div className={styles.navHeader}>
+          <img src={logo} alt="backroads logo" />
+          <button type="button" className={styles.logoBtn} onClick={toggleNav}>
+            <FaAlignRight className={StyleSheetList.logoIcon} />{" "}
+          </button>
+        </div>
+        <ul
+          className={
+            isOpen
+              ? `${styles.navLinks} ${styles.showNav}`
+              : `${styles.navLinks}`
+          }
+        >
+          {links.map((item, i) => {
+            return (
+              <li key={i}>
+                <Link to={item.path}>{item.text}</Link>
+              </li>
+            )
+          })}
+        </ul>
+<div className={styles.navSocialLinks}>{socialIcons.map((item, i) => {
+  return (
+    <a key={i} href={item.url} target="_blank" rel="noopener noreferrer">
+      {item.icon}
+    </a>
+  )
+})}</div>
+      </div>
+    </nav>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { FaAdobe } from "react-icons/fa"
 
 export default () => (
   <Layout>
     <div>Hello from backroads!</div>
-    <FaAdobe />
   </Layout>
 )
